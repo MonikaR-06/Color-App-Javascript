d5cba304f4dd75bafd95150d93009152a6d1876f
monikaa06 <writetomonika06@gmail.com>
2024-12-01 20:11:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const handleBlue = () => {
+    document.getElementById("cir").style.backgroundColor = "blue"
+}
+
+const handleRed = () => {
+    document.getElementById("cir").style.backgroundColor = "red"
+}
+
+const handleGreen = () => {
+    document.getElementById("cir").style.backgroundColor = "green"
+}
+
+const handleReset = () => {
+    document.getElementById("cir").style.backgroundColor = "white"
+    document.getElementById("cir").style.borderRadius = "50%"
+}
+
+const handleColor = () => {
+    const color = document.getElementById("color").value
+    document.getElementById("cir").style.backgroundColor = color
+}
+
+const handleSquare = () => {
+    document.getElementById("cir").style.borderRadius = "0%"
+}
+
+const handleRectangle = () => {
+    document.getElementById("cir").style.borderRadius = "0%"
+    document.getElementById("cir").style.width = "600px" 
+}
+
+const handleTraingle = () => {
+    document.getElementById("cir").style.width = "600px"
+    document.getElementById("cir").style.borderRadius = "50%"
+}
